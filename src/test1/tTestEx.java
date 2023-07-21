8bbe033bea2df02bd46a2abc97c5f9df2716d7ae
K1plangatf <k.fredrick24.com>
2023-07-20 20:28:28 -0500

    third commit

--- src/test1/tTestEx.java
+++ src/test1/tTestEx.java
@@ -6,5 +6,6 @@ public class tTestEx {
 		System.out.println("hello.");
 		System.out.println("day 2 of test");
 		System.out.println("wrote 2 lines of code");
+		System.out.println("Third line of code");
 	}
 }

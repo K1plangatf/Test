3888a95ce2cf5894bd352d928ac813e6a84ad1e4
suman937 <sumanlamichhane937@gmail.com>
2023-07-21 21:51:02 -0400

    deleted some lines of code

--- src/test1/tTestEx.java
+++ src/test1/tTestEx.java
@@ -3,11 +3,11 @@ package test1;
 public class tTestEx {
 
 	public static void main(String Args []) {
-		System.out.println("hello.");
+	
 		System.out.println("day 2 of test");
 		System.out.println("wrote 2 lines of code");
 		System.out.println("si has change your code");
-		System.out.println("add extra line for feature1 branch");
+		
 	
 	}
 }
